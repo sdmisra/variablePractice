9b58cc504b8dca881cc4d37baf8d77ea1d9d1fb9
Shane Misra <shane.misra@gmail.com>
2022-11-08 18:07:32 -0700

    Add numbers

--- variables.js
+++ variables.js
@@ -1,5 +1,11 @@
 // Variable Practice!
+// STRINGS
 var name = "Shane"
 var aptLocation = "Broomfield, CO"
 var famLocation = "Naperville, IL"
 var birthPlace = "Joliet, IL"
+// NUMBERS
+var currentAge = 32
+var currentWeight = 145
+var numberOfEyes = 2
+var wisdomTeethRemaining = 0
