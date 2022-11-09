15ba5a2357240f64072c858086be32541616139a
Shane Misra <shane.misra@gmail.com>
2022-11-08 18:22:42 -0700

    Add modified strings and fun phrases

--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
 // Variable Practice!
 // STRINGS
-var name = "Shane"
+var myName = "Shane"
 var aptLocation = "Broomfield, CO"
 var famLocation = "Naperville, IL"
 var birthPlace = "Joliet, IL"
@@ -14,3 +14,12 @@ var isMarried = true
 var isHungry = true
 var hasWorkedRetail = true
 var willReturnRetail = false
+// Note for line 20: I found that if I use just `name = "Shane"` above, on line 18 I get an error when trying to reassign `name`.
+// It must be a word that is reserved by JavaScript.
+// If I were to join a witness protection program:
+myName = "Gary"
+aptLocation = "San Diego, CA"
+famLocation = "Denver, CO"
+console.log(`If I were to go into witness protection I might need to go by ${myName}.`)
+console.log(`My current weight is ${currentWeight}...or around there.`)
+console.log(`Throughout my entire life, I have had ${numberOfEyes} eyes, and for that I am grateful.`)
